626616592638ed5ec4d7e52309fd6efa54efdeb7
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2022-12-12 05:25:59 -0800

    Update xcat_customize_alma.sh

--- xcat_customize_alma.sh
+++ xcat_customize_alma.sh
@@ -23,4 +23,22 @@ sed -i 's/1636882174.934804/1652294731.711015' /opt/xcat/lib/perl/xCAT/data/disc
 sed -i 's/rocky8.4/alma8.6/g' /opt/xcat/lib/perl/xCAT/data/discinfo.pm   
 sed -i 's/1624205633.869423/1652294731.711601' /opt/xcat/lib/perl/xCAT/data/discinfo.pm    #minimal.iso
 
+# add some files related to xcat
+
+mkdir -p xCAT-server/share/xcat/install/alma/
+mv compute.alma8.pkglist xCAT-server/share/xcat/install/alma/compute.alma8.pkglist
+mv compute.alma8.tmpl xCAT-server/share/xcat/install/alma/compute.alma8.tmpl
+mv service.alma8.pkglist xCAT-server/share/xcat/install/alma/service.alma8.pkglist
+mv service.alma8.tmpl xCAT-server/share/xcat/install/alma/service.alma8.tmpl
+mv service.alma8.x86_64.otherpkgs.pkglist xCAT-server/share/xcat/install/alma/service.alma8.x86_64.otherpkgs.pkglist
+
+
+mkdir -p xCAT-server/share/xcat/netboot/alma/
+mv compute.alma8.x86_64.exlist xCAT-server/share/xcat/netboot/alma/compute.alma8.x86_64.exlist
+mv compute.alma8.x86_64.pkglist xCAT-server/share/xcat/netboot/alma/compute.alma8.x86_64.pkglist
+mv compute.alma8.x86_64.postinstall  xCAT-server/share/xcat/netboot/alma/compute.alma8.x86_64.postinstall
+
+mv dracut_047 xCAT-server/share/xcat/netboot/alma/dracut_047
+touch xCAT-server/share/xcat/netboot/alma/genimage
+mv geninitrd xCAT-server/share/xcat/netboot/alma/geninitrd
 
