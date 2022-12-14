cd467973b7a9681d8241968fa662dbe0b001e3ab
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2022-12-13 21:01:01 -0800

    Update xcat_customize_alma.sh

--- xcat_customize_alma.sh
+++ xcat_customize_alma.sh
@@ -19,13 +19,13 @@ sed -i 's/rocky/alma/g' /opt/xcat/lib/perl/xCAT/Utils.pm
 sed -i 's/rocky/alma/g' /opt/xcat/lib/perl/xCAT_plugin/anaconda.pm
 sed -i 's/Rocky/Alma/g' /opt/xcat/lib/perl/xCAT_plugin/anaconda.pm
 sed -i 's/rocky8.5/alma8.6/g' /opt/xcat/lib/perl/xCAT/data/discinfo.pm
-#sed -i 's/1636882174.934804/1652294731.711015' /opt/xcat/lib/perl/xCAT/data/discinfo.pm    #dvd.iso
+sed -i 's/1636882174.934804/1652294731.711015/g' /opt/xcat/lib/perl/xCAT/data/discinfo.pm    #dvd.iso
 sed -i 's/rocky8.4/alma8.6/g' /opt/xcat/lib/perl/xCAT/data/discinfo.pm   
-#sed -i 's/1624205633.869423/1652294731.711601' /opt/xcat/lib/perl/xCAT/data/discinfo.pm    #minimal.iso
+sed -i 's/1624205633.869423/1652294731.711601/g' /opt/xcat/lib/perl/xCAT/data/discinfo.pm    #minimal.iso
 
 # add some files related to xcat
 
-mkdir -p xCAT-server/share/xcat/install/alma/
+mkdir -p /opt/xcat/share/xcat/install/alma/
 mv compute.alma8.pkglist /opt/xcat/share/xcat/install/alma/compute.alma8.pkglist
 mv compute.alma8.tmpl /opt/xcat/share/xcat/install/alma/compute.alma8.tmpl
 mv service.alma8.pkglist /opt/xcat/share/xcat/install/alma/service.alma8.pkglist
@@ -33,7 +33,7 @@ mv service.alma8.tmpl /opt/xcat/share/xcat/install/alma/service.alma8.tmpl
 mv service.alma8.x86_64.otherpkgs.pkglist /opt/xcat/share/xcat/install/alma/service.alma8.x86_64.otherpkgs.pkglist
 
 
-mkdir -p xCAT-server/share/xcat/netboot/alma/
+mkdir -p /opt/xcat/share/xcat/netboot/alma/
 mv compute.alma8.x86_64.exlist /opt/xcat/share/xcat/netboot/alma/compute.alma8.x86_64.exlist
 mv compute.alma8.x86_64.pkglist /opt/xcat/share/xcat/netboot/alma/compute.alma8.x86_64.pkglist
 mv compute.alma8.x86_64.postinstall  /opt/xcat/share/xcat/netboot/alma/compute.alma8.x86_64.postinstall
